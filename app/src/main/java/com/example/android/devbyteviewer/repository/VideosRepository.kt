fac4aeb259f10bbb054ddf3b2cf3749da22ea15e
Zaela24 <zaela.marie.rose@gmail.com>
2020-05-06 21:55:01 -0500

    Finished offline cache by implementing a repository

--- app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -16,3 +16,37 @@
  */
 
 package com.example.android.devbyteviewer.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.database.asDomainModel
+import com.example.android.devbyteviewer.domain.Video
+import com.example.android.devbyteviewer.network.Network
+import com.example.android.devbyteviewer.network.asDatabaseModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/**
+ * Fetches devbyte videos from network and stores them on disk.
+ * Dependency injection via requiring database as argument (i.e. specifies and forces Context)
+ */
+class VideosRepository(private val database: VideosDatabase) {
+
+    /**
+     * Uses Transformations map to switch LiveData to Video, which can be displayed on screen
+     */
+    val videos: LiveData<List<Video>> = Transformations.map(database.videoDao.getVideos()) {
+        it.asDomainModel()
+    }
+
+    /**
+     * Used to launch Coroutine on IO thread to update database videos from network
+     */
+    suspend fun refreshVideos() {
+        withContext(Dispatchers.IO) {
+            val playlist = Network.devbytes.getPlaylist().await()
+            database.videoDao.insertAll(*playlist.asDatabaseModel())
+        }
+    }
+}
